1a172864ae71e23c9853dd61dce66476954168dc
Amir-Studio <kabiri1489@gmail.com>
2026-08-21 23:44:53 +0330

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ============================================================
-// CONFIG — تنظیمات MOON SHOP
-// ============================================================
-const CONFIG = {
-    cardNumber: '6219861956267685',
-    adminSoroushId: '@Sjjshh',
-    supportLink: 'https://soroush-plus.com/support'
-};
